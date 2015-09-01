f68436b8cabf9ee0664316acf442434f4e81fe02
jacobdlewis <jacobdlewis@gmail.com>
2015-09-01 00:47:18 -0500

    can create courses. simple form installed

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 # Use sqlite3 as the database for Active Record
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,18 +1,33 @@
 class CoursesController < ApplicationController
 
   def new
+    @course = Course.new
   end
 
   def index
+    @courses = Course.all
   end
 
   def create
+    @course = Course.new(courses_params)
+    if @course.save
+      redirect_to root_path, notice: "Your entry for #{@course.name} was saved successfully!"
+    else
+      flash.alert = "Please fix the errors below to continue."
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  protected
+
+  def courses_params
+    params.require(:course).permit(:name, :city, :state, :number_of_holes)
   end
 
 end
